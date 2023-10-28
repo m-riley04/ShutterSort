68e8c884c5790314d487e692cdb02cc8dddadef5
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-10-28 14:23:32 -0500

    Move code into src folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <fstream> 
 #include <string> 
 #include <filesystem> 
+#include "LocalImage.h"
 
+using namespace std;
+namespace fs = filesystem;
 
-//defined class called Sort
+// defined class called Sort
 class Sort{
-    public: 
-
-        //member functions 
+    public:
+        // member functions 
         void sortToMisc(const std::string& origionalDirectory, const std::string& miscDirectory){
 
             for (std::filesystem::directory_entry entry : std::filesystem::directory_iterator(origionalDirectory)){ // A "for each" loop that iterates through each entry
@@ -30,7 +32,13 @@ class Sort{
 
 
 int main(){
+    // Initialize 
+    fs::path anchorDirectory("C:\\Users\\vex10\\Desktop\\TestingDirectory\\newimg.png");
+
+    LocalImage image(anchorDirectory);
     
+
+    return 0;
 }
 
 
