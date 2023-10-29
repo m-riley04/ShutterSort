393de8629eee95522ed059a23c6e3877cc10a681
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-10-28 21:21:26 -0500

    added experimental file iterating

--- src/anchor.cpp
+++ src/anchor.cpp
@@ -1,95 +1,86 @@
-#include <iostream>
-#include <filesystem>
 #include <thread>
 #include <Windows.h>
-#include <cstring>
+#include "anchor.h"
 
 using namespace std;
 namespace fs = filesystem;
 
-/* Checks for any new files in the input folder. If there are files, moves them to the output folder. Checks after a certain time interval passes.*/
-void updateDirectory(const fs::path input, const fs::path output, const int milliseconds)
+// Iterates through the anchor directory, checks for any changes, and calls on the sorter to sort if there are.
+void Anchor::update()
 {
-    while (1)
+    // Check if the directory is empty. If it is, do nothing.
+    if (fs::is_empty(input))
     {
-        // Check if the directory is empty. If it is, do nothing.
-        bool empty = fs::is_empty(input);
-        if (empty)
-        {
-            Sleep(milliseconds);
-            continue;
-        }
-
-        /* Move all non-image entries to the miscellaneous directory (Colin's function)*/
-        //foo(bar)
-
-        // Loop through every file in the input directory with a directory_iterator
-        for (fs::path const &dir_entry : std::filesystem::directory_iterator{input}) {
-            fs::path newOutput = output;
-            fs::path name = dir_entry.filename();
-            
-            // Concat file name to output path
-            newOutput /= name;
-
-            // Print moving file
-            cout << "File found: " << name << endl;
-
-            /*  This is where the sorting part will take place. */
-            // Create LocalImage object 
-            //Exiv2::Image exivImage = Exiv2::ImageFactory(dir_entry);
-            //LocalImage image(dir_entry, exivImage)
-
-            // Move file to the output directory
-            fs::rename(dir_entry, newOutput);
-        }
-
-        // Tell the user it is finished updating
-        cout << "Updated directory." << input.string() << endl;
-        Sleep(milliseconds);
-    }
-}
-
-int main()
-{
-    // Set your input folder path
-    const fs::path inputDirectory{"C:\\Users\\vex10\\Desktop\\TestingDirectory"};
-
-    // Set your output folder path
-    const fs::path outputDirectory{"C:\\Users\\vex10\\Desktop\\OutputDirectory"};
-
-    // Initialize thread
-    thread sortingThread(updateDirectory, inputDirectory, outputDirectory, 1000);
-
-    // Sorting status
-    bool isRunning = true;
-
-    // Get user input to control sorting
-    int userInput = 0;
-    while (userInput != 2)
-    {
-        cout << "1) Start/Stop Sorting\n2) Quit" << endl << ">> ";
-        cin >> userInput;
-
-        if (userInput == 1 && !isRunning)
-        {
-            //thread sortingThread(updateDirectory, inputDirectory, outputDirectory, 1000);
-            cout << "Sorting has been started." << endl;
-            isRunning = true;
-        }
-        else if (userInput == 1 && isRunning)
-        {
-            //sortingThread.detach();
-            cout << "Sorting has been stopped." << endl;
-            isRunning = false;
-        }
+        break;
     }
 
-    // Detach sorting thread
-    sortingThread.detach();
+    /* Move all non-image entries to the miscellaneous directory (Colin's function)*/
+    //foo(bar)
 
-    // Wait before immedietly closing program
-    cout << "Thank you for sorting!" << endl;
-    std::cin.ignore();
-    
-    return 0;
+    // Loop through every file in the input directory with a directory_iterator
+    for (fs::path const &dir_entry : std::filesystem::directory_iterator{directory}) {
+        // Get the name of the current file
+        fs::path name = dir_entry.filename();
+
+        // Print file that is being moved
+        cout << "File found: " << name << endl;
+
+        // Create LocalImage object 
+        //Exiv2::Image exivImage = Exiv2::ImageFactory(dir_entry);
+        //LocalImage image(dir_entry, exivImage)
+
+        // Call on the sorter
+
+        // Get the sorted folder name
+        //fs::path output = output;
+
+        // Concat file name to sorted folder
+        //outputFolder /= name;
+
+        // Concat sorted folder to main output folder
+        newOutput /= output
+
+        // Move file to the main output folder
+        fs::rename(dir_entry, newOutput);
+    }
+
+    // Tell the user it is finished updating
+    cout << "Updated directory." << input.string() << endl;
 }
+
+/*// Iterates through a given input and output directory and calls on the sorter to sort. 
+void Anchor::update(const fs::path input, const fs::path output)
+{
+    // Check if the directory is empty. If it is, do nothing.
+    if (fs::is_empty(input))
+    {
+        break;
+    }
+
+    //--- Move all non-image entries to the miscellaneous directory (Colin's function)
+    //foo(bar)
+
+    // Loop through every file in the input directory with a directory_iterator
+    for (fs::path const &dir_entry : std::filesystem::directory_iterator{input}) {
+        fs::path newOutput = output;
+        fs::path name = dir_entry.filename();
+        
+        // Concat file name to output path
+        newOutput /= name;
+
+        // Print moving file
+        cout << "File found: " << name << endl;
+
+        //--- This is where the sorting part will take place. 
+        // Create LocalImage object 
+        //Exiv2::Image exivImage = Exiv2::ImageFactory(dir_entry);
+        //LocalImage image(dir_entry, exivImage)
+
+        // Move file to the output directory
+        fs::rename(dir_entry, newOutput);
+    }
+
+    // Tell the user it is finished updating
+    cout << "Updated directory." << input.string() << endl;
+}
+*/
--- src/anchor.h
+++ src/anchor.h
@@ -1,15 +1,35 @@
 #ifndef ANCHOR_H
 #define ANCHOR_H
 
-#include <sorter.h>
+#include <filesystem>
 
 class Anchor {
     private:
-        
+        std::filesystem::path directory;
+        std::filesystem::path outputDirectory;
+        int updateInterval = 1000;
 
     public:
-        /* Default constructor*/
+        // Default constructor
         Anchor();
+        
+        /*  
+            Checks for any new files in the anchor folder. 
+            If there are files, calls on the sorter and moves them to the output folder. 
+        */
+        void update();
+
+        /* 
+            Checks for any new files in the given input folder. 
+            If there are files, calls on the sorter and moves them to the given output folder. 
+        */
+        void update(const std::filesystem::path input, const std::filesystem::path output);
+
+        // Returns the path to the anchor directory
+        void getPath();
+
+        // Returns the update interval (in milliseconds)
+        void getInterval();
 };
 
 #endif
