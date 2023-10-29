c733bc28d1a70a156daeefbfbd00b8dae238d844
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-10-28 21:20:20 -0500

    Added test and include major classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+// Include all main classes for clarity (Will be modified/removed later)
 #include "localimage.h"
 #include "sorter.h"
+#include "sortingmethod.h"
+#include "anchor.h"
 
 int main(){
-
+    // Test to see if main executes successfully.
+    printf("Hello, world!");
 
     return 0;
 }
