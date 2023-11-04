a664515889d74d785299b3218dfc4f818ab39f27
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-03 22:00:22 -0500

    added and rearranged values

--- src/sortingmethod.h
+++ src/sortingmethod.h
@@ -8,21 +8,51 @@
 
 class SortingMethod {
     private:
+        std::string name;
         std::string tag;
-        int minValue;
-        int maxValue;
-
-        int status = DISABLED;
+        int min;
+        int max;
+        int status;
 
     public:
-        /* Default constructor */
+        // Constructor for the SortingMethod class
         SortingMethod();
+        SortingMethod(std::string &name, std::string &tag, int min, int max);
 
-        /* Parameterized constructor */
-        SortingMethod(std::string &tag);
+        /* --- GETTERS --- */
+        // Returns the name of the folder that this sotring method's metadata tag belongs to
+        std::string getName();
 
-        /* Returns the SortingMethod's tag/name */
+        // Returns the metadata tag
         std::string getTag();
+
+        // Returns the minimum bound
+        int getMin();
+
+        // Returns the maximum bound
+        int getMax();
+
+        // Returns an array containing the upper and lower bounds
+        int *getBounds();
+
+        /* Returns the status of the sorting method. 0 = disabled, 1 = enabled */
+        int getStatus();
+
+        /* --- SETTERS --- */
+        // Sets the name of the sorting method (and the corresponding folder)
+        void setName(std::string name);
+
+        // Sets the metadata tag
+        void setTag(std::string tag);
+
+        // Sets the minimum bound
+        void setMin(int min);
+
+        // Sets the maximum bound
+        void setMax(int max);
+
+        // Sets the status (0 = disabled, 1 = enabled)
+        void setStatus(int status);
 };
 
 #endif
