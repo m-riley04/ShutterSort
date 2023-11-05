e658d8688fc6a18396ea96476de67daad9b2efe2
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-05 15:39:20 -0600

    create metadata class for storing exif metadata

---
+++ src/metadata.h
@@ -0,0 +1,41 @@
+#ifndef METADATA_H
+#define METADATA_H
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <exiv2/exiv2.hpp>
+
+class Metadata {
+    private:
+    // Using a map to store key-value pairs of metadata.
+    std::map<std::string, std::string> data;
+
+    public:
+        // Adds or updates a metadata value.
+        void set(const std::string& key, const std::string& value);
+
+        // Retrieves a metadata value, returns an empty string if not found.
+        std::string get(const std::string& key);
+
+        // Loads a metadata with values
+        void Metadata::load(std::string path);
+        void Metadata::load(Exiv2::ExifData data);
+
+        // Checks if a metadata key exists.
+        bool contains(const std::string& key);
+
+        // Removes a metadata key-value pair.
+        void remove(const std::string& key);
+
+        // Function to clear all metadata.
+        void clear();
+
+        // Function to get the total number of metadata entries.
+        size_t size();
+
+        // A function to print all metadata for debugging purposes.
+        void printAll();
+};
+
+#endif
