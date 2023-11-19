fbba959b6f0fbad76f1db3ea982400a1e5397475
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-19 15:33:52 -0600

    added constructor and integer-type sorting

--- src/anchor.h
+++ src/anchor.h
@@ -2,18 +2,21 @@
 #define ANCHOR_H
 
 #include <filesystem>
+#include <string>
 #include "sorter.h"
 
 class Anchor {
     private:
         std::filesystem::path directory;
         std::filesystem::path outputDirectory;
-        int updateInterval = 1000;
         Sorter sorter;
-
+        int updateInterval = 1000;
+        
     public:
         // Default constructor
         Anchor();
+        Anchor(std::string directory, std::string outputDirectory, Sorter& sorter, int updateInterval = 1000);
+        Anchor(std::filesystem::path directory, std::filesystem::path outputDirectory, Sorter& sorter, int updateInterval = 1000);
         
         /*  
             Checks for any new files in the anchor folder. 
@@ -27,11 +30,19 @@ class Anchor {
         */
         void update(const std::filesystem::path input, const std::filesystem::path output);
 
+
+        /* --- GETTERS --- */
         // Returns the path to the anchor directory
-        void getPath();
+        std::filesystem::path getDirectory();
+
+        // Returns the output directory for the anchor
+        std::filesystem::path getOutputDirectory();
+        
+        // Returns the sorter object for the anchor
+        Sorter getSorter();
 
         // Returns the update interval (in milliseconds)
-        void getInterval();
+        int getInterval();
 };
 
 #endif
