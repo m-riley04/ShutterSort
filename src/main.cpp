753280bdc5d03223e21ba1b249b70285378d212d
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-19 18:07:45 -0600

    consolidate main into application object

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,10 @@
-// Include all main classes for clarity (Will be modified/removed later)
-#include "localimage.h"
-#include "sorter.h"
-#include "sortingmethod.h"
-#include "anchor.h"
-#include "metadata.h"
-
-#include <exiv2/exiv2.hpp>
-#include <iostream>
-#include <regex>
+#include "application.h"
 
 int main(){
-    // Test to see if main executes successfully.
-    printf("Hello, world!\n");
+    Application app;
 
-    // Get image path
-    std::string imgPath = "TestCases/cat.jpg";
-
-    // Load metadata object
-    Metadata myMetadata;
-    myMetadata.load(imgPath);
-    
-    LocalImage image(imgPath);
-    image.getMetadata().printAll();
-
-    // Wait to close program
-    std::cin.ignore();
+    // Start the application
+    app.run();
 
     return 0;
 }
-
