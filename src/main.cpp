1276318035d197b7055fa16f278ad5e06b4f3212
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-25 22:57:11 -0600

    Formatted main() to start the qt runtime loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
-#include "application.h"
+#include "mainwindow.h"
+#include <QtWidgets/QApplication>
 
-int main(){
-    Application app;
-
-    // Start the application
-    app.run();
-
-    return 0;
+int main(int argc, char* argv[])
+{
+    // Initialize the Qt application class
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
 }
+
+//#include "application.h"
+//#include <QtWidgets/QApplication>
+//#include "mainwindow.h"
+//
+//int main(){
+//    Application app;
+//
+//    QApplication a();
+//
+//    // Start the application
+//    app.run();
+//
+//    return 0;
+//}
