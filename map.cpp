478dfe7fc2fdb14dc083c337a5e53c1f2d2d068f
Sam Aven <samjaven33@gmail.com>
2023-12-03 19:18:22 -0600

    Creating map
    map

---
+++ map.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int main() {
+    // Define the hash table
+    std::unordered_map<std::string, std::string> exifTags;
+
+    // Inserting tags into the hash table
+    exifTags["NewSubfileType"] = "exif.image.NewSubfileType";
+    exifTags["SubfileType"] = "exif.image.SubfileType";
+    exifTags["ImageWidth"] = "exif.image.ImageWidth";
+    exifTags["ImageLength"] = "exif.image.ImageLength";
+    exifTag["Compression"] = "exif.image.Compression";
+    exifTag["PhotometricInterpretation"] = "exif.image.PhotometricInterpretation";
+    exifTag["Thresholding"] = "exif.image.Thresholding";
+    exifTag["CellWidth"] = "exif.image.CellWidth";
+    exifTag["CellLength"] = "exif.image.CellLength";
+    exifTag["FillOrder"] = "exif.image.FillOrder";
+    exifTag["StripOffsets"] = "exif.image.StripOffsets";
+    exifTag["Orientation"] = "exif.image.Orientation";
+    exifTag["SamplesPerPixel"] = "exif.image.SamplesPerPixel";
+    exifTag["RowsPerStrip"] = "exif.image.RowsPerStrip";
+    exifTag["XResolution"] = "exif.image.XResolution";
+    exifTag["YResolution"] = "exif.image.YResolution";
+    exifTag["PlanarConfiguration"] = "exif.image.PlanarConfiguration";
+    exifTag["XPosition"] = "exif.image.XPosition";
+    exifTag[""]
+}
