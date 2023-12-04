6ca054a21b53df2ecf2d510e73e147ea092ed089
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-12-03 18:46:58 -0600

    remove unnecessary method definitions and comments

--- src/Sorter.h
+++ src/Sorter.h
@@ -1,9 +1,7 @@
 #ifndef SORTER_H
 #define SORTER_H
 
-#include <string>
 #include <list>
-#include <iostream>
 #include "sortingmethod.h"
 
 class Sorter {
@@ -12,15 +10,6 @@ class Sorter {
         std::list<SortingMethod> methods;
 
     public:
-        /* Returns the filesystem::path object for the image */
-        void sortToMisc(const std::string& origionalDirectory, const std::string& miscDirectory);
-        void makeTempCopy(const std::string& sourcePath, const std::string& destPath);
-        void deleteTempCopy(const std::string& destPath);
-        bool sort(int target, int minRange, int maxRange); // integer
-        //bool sort(Metadata data, float minRange, float maxRange, const std::string & key);//float
-        //bool sort(Metadata data, const std::string& str, const std::string & key);//string
-        //bool sort();//date
-        //bool sort();//time
 
         /* --- GETTERS --- */
         std::list<SortingMethod>& getMethods();
